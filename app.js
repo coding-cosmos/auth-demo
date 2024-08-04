fd08461aeccea2c68bc5ca14ba98bbab9a88ab34
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-08-04 12:23:18 +0530

    Add middlewares

---
+++ app.js
@@ -0,0 +1,26 @@
+const {Pool} = require('pg');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pool = new Pool({
+    connectionString:process.env.CONNECTION_STRING
+});
+
+const app = express();
+
+app.set('views',__dirname);
+app.set('view engine ','ejs');
+
+
+app.use(session({secret:'cats',resave:false,saveUninitialized:false}));
+app.use(passport.session());
+app.use(express.urlencoded({extended:false}));
+
+
+app.get('/',(req,res)=> res.render('index'));
+
+app.listen(3000,()=> console.log('app listening on port 3000!'));
