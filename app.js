cf4a85f5aa59e407f1ec082a3062ffdc86be05fd
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-08-04 12:59:18 +0530

    Add req.user as a parameter for index template

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(async (id,done)=>{
     }
 });
 
-app.get('/',(req,res)=> res.render('index'));
+app.get('/',(req,res)=> res.render('index',{user:req.user}));
 app.get('/sign-up',(req,res) => res.render('sign-up-form'));
 app.post('/sign-up',async (req,res,next)=>{
     try{
